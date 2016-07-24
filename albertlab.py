c55859f5b9ba32147ee8dbc5deae8181a5f65834
Ares <qq79402005@gmail.com>
2016-07-25 02:46:43 +0800

    添加静态数据与模板数据

--- albertlab.py
+++ albertlab.py
@@ -14,14 +14,48 @@ settings.configure(
     ),
 )
 
+from django import forms
 from django.conf.urls import url
 from django.http import HttpResponse
+
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+class ImageForm(forms.Form):
+    height = forms.IntegerField(min_value=1, max_value=2048)
+    width = forms.IntegerField(min_value=1, max_value=2048)
+
+    def generate(self, image_format='PNG'):
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image = Image.new('RGB', (width, height))
+        draw = ImageDraw.Draw(image)
+        text = '{} X {}'.format(width, height)
+        textwidth, textheight = draw.textsize(text)
+        if(textwidth<width and textheight<height):
+            draw.text((0, 0), text, fill=(255,255,255))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
+
+def placeholder(request, width, height):
+    # TODO Rest of the view will go here
+    form = ImageForm({'height':height, 'width':width})
+    if form.is_valid():
+        image = form.generate()
+
+        return HttpResponse(image, content_type='image/png')
+    else :
+        return HttpResponseBadRequest('Invalid Image Request')
 
 def index(request):
     return HttpResponse('Hello World')
 
+# url pattern fot the server root.
 urlpatterns = (
-    url(r'^$', index),
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
+    url(r'^$', index, name='homepage'),
 )
 
 # WSGI
